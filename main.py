31fd9d1b69c20e617cd653875a46c77c049cd3ab
ssong0320 <118571777+ssong0320@users.noreply.github.com>
2024-09-29 09:09:04 -0400

    create csv and write to it

---
+++ main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import csv
+from datetime import datetime
+
+
+class CSV:
+    CSV_FILE = "finiance_data.csv"
+    COLUMNS = ["date", "amount", "category", "description"]
+
+    @classmethod
+    def initialize_csv(cls):
+        try:
+            pd.read_csv(cls.CSV_FILE)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=cls.COLUMNS)
+            df.to_csv(cls.CSV_FILE, index=False)
+
+    @classmethod
+    def add_entry(cls, date, amount, category, description):
+        new_entry = {
+            "date": date,
+            "amount": amount,
+            "category": category,
+            "description": description,
+        }
+
+        with open(cls.CSV_FILE, "a", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
+            writer.writerow(new_entry)
+        print("new entry added")
+
+
+CSV.initialize_csv()
+CSV.add_entry("20-07-2024", 125.65, "Income", "Salary")
