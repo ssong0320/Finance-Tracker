4d0f62553b3b5d3045eb12de8d089860904df684
ssong0320 <118571777+ssong0320@users.noreply.github.com>
2024-09-30 22:33:07 -0400

    data entry getter methods

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import csv
 from datetime import datetime
+from data_entry import get_amount, get_category, get_date, get_description
 
 
 class CSV:
@@ -30,5 +31,16 @@ class CSV:
         print("new entry added")
 
 
-CSV.initialize_csv()
-CSV.add_entry("20-07-2024", 125.65, "Income", "Salary")
+def add():
+    CSV.initialize_csv
+    date = get_date(
+        "Enter the date of the transaction (dd-mm-yyyy) or enter for today's date: ",
+        allow_default=True,
+    )
+    amount = get_amount()
+    category = get_category()
+    description = get_description()
+    CSV.add_entry(date, amount, category, description)
+
+
+add()
